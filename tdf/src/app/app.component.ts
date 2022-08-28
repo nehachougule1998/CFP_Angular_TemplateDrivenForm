b70b651aa4d3ee626b24aff24af975024702ba41
nehachougule1998 <nchougule734@gmail.com>
2022-08-28 09:44:07 +0530

    TemplateDrivenForm

---
+++ tdf/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService }from './enrollment.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
+  submitted = false;
+  errorMsg = '';
+
+  userModel = new User('Neha', 'neha23@gmail.com', 9876543210,'default', 'morning', true);
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+  validateTopic(value: any) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit(userForm: any) {
+    console.log(userForm);
+  //  this.submitted = true;
+  //  this._enrollmentService.enroll(this.userModel)
+  //  .subscribe(
+  //   data => console.log('Success!', data),
+  //   error => this.errorMsg = error.statusText
+  //  )
+
+  }
+}
+
